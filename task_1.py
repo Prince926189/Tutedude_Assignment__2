ffb5997beed863589603cb75049a51f42fc07830
Prince926189 <panditpeince88435@gmail.com>
2025-09-29 16:26:31 +0530

    First commit - assignment 2

---
+++ task_1.py
@@ -0,0 +1,7 @@
+#task 1 
+a = int(input("Enter the number : "))
+
+if a%2 == 0:
+    print(a," is an Even number.")
+else:
+     print(a," is an Odd number.")
---
+++ task_2.py
@@ -0,0 +1,5 @@
+#tak 2
+sum = 0 
+for i in range(0,50+1):
+    sum = sum + i 
+print("The sum of 1 to 50 is :",sum)
